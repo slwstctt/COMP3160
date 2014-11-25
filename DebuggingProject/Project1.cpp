91a4cf466d356b2c981650e23b8953e845781a99
Shawn Westcott <slwstctt@memphis.edu>
2014-11-25 13:42:18 -0600

    Create Project1.cpp

---
+++ DebuggingProject/Project1.cpp
@@ -0,0 +1,47 @@
+// This program averages a series
+// of numbers, except that it contains
+// a fatal bug.
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int nNumberofArgs, char *pszArgs[])
+{
+	cout << "This program is designed to crash!"
+		   << endl;
+
+	int nSum;
+	int nNums;
+
+	// accumulate input numbers until the
+	// user enteres a negative number, then
+	// return the average
+	nNums = 0;
+	while(true)
+	{
+		// enter another number to add
+		int nValue;
+		cout << "Enter another number:";
+		cin >> nValue;
+		cout << endl;
+
+		// if the input number is negative...
+		if(nValue < 0)
+		{
+			// ... then output the average
+			cout << "Average is: "
+				 << nSum/nNums
+				 << endl;
+			break;
+		}
+
+		// not negative, add the value to
+		// the accumulator
+		nSum += nValue;
+	}
+
+	cin.ignore(10000, '\n');
+	return 0;
+}
